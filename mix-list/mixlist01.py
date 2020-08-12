fa171083deb9240c7439454c282cd05d0ca10f8d
Ousmane Toure <digitalera21@gmail.com>
2020-08-12 10:35:19 -0400

    latest changes

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#create a list containing three items
+my_list = [ "192.168.0.5", 5060, "UP" ]
+#code to return the IP address
+print("the first item in the list (IP): " + my_list[0])
+#code to return the port number
+print("The second item in the list (port): " + str(my_list[1]) )
+#code to return the last item 
+print("The last item in the list (state): " + my_list[2] )
+new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+print("when I " + new_list[5] + " into IP addresses " + new_list[3] + " or " + new_list[4] + " I am unable to ping ports " + str(new_list[0]) + "," + new_list[1] + "," + str( new_list[2]) + ".")
+
+#print(f"When I ssh into IP addresses {new_list[3]} or {new_list[4]} I am unable to ping ports {new_list[0]}, {new_list[1]}, or {new_list[2]}")
